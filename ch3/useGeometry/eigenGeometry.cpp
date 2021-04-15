c5366b6fc9f8117f938e501bad1034a279909f39
RingWong <285986840@qq.com>
2021-04-15 09:28:37 +0800

    add commend

--- ch3/useGeometry/eigenGeometry.cpp
+++ ch3/useGeometry/eigenGeometry.cpp
@@ -14,17 +14,25 @@ int main(int argc, char **argv) {
 
   // Eigen/Geometry 模块提供了各种旋转和平移的表示
   // 3D 旋转矩阵直接使用 Matrix3d 或 Matrix3f
+  // 用单位阵赋值
   Matrix3d rotation_matrix = Matrix3d::Identity();
   // 旋转向量使用 AngleAxis, 它底层不直接是Matrix，但运算可以当作矩阵（因为重载了运算符）
+  // AngleAxisd 表示数据类型是double
+  // 任意旋转都可以用一个旋转轴和旋转角表示，于是有
   AngleAxisd rotation_vector(M_PI / 4, Vector3d(0, 0, 1));     //沿 Z 轴旋转 45 度
+  // 设置当前输出精度为3位小数
   cout.precision(3);
   cout << "rotation matrix =\n" << rotation_vector.matrix() << endl;   //用matrix()转换成矩阵
   // 也可以直接赋值
+  // 将旋转向量转换为旋转矩阵。应用罗德里格斯公式？
   rotation_matrix = rotation_vector.toRotationMatrix();
+
   // 用 AngleAxis 可以进行坐标变换
+  // 对v向量进行一次旋转
   Vector3d v(1, 0, 0);
   Vector3d v_rotated = rotation_vector * v;
   cout << "(1,0,0) after rotation (by angle axis) = " << v_rotated.transpose() << endl;
+
   // 或者用旋转矩阵
   v_rotated = rotation_matrix * v;
   cout << "(1,0,0) after rotation (by matrix) = " << v_rotated.transpose() << endl;
--- ch4/useSophus.cpp
+++ ch4/useSophus.cpp
@@ -12,9 +12,12 @@ using namespace Eigen;
 int main(int argc, char **argv) {
 
   // 沿Z轴转90度的旋转矩阵
+  // Eigen::AngleAxisd定义旋转向量
+  // M_PI是math.h定义的宏
   Matrix3d R = AngleAxisd(M_PI / 2, Vector3d(0, 0, 1)).toRotationMatrix();
   // 或者四元数
   Quaterniond q(R);
+
   Sophus::SO3d SO3_R(R);              // Sophus::SO3d可以直接从旋转矩阵构造
   Sophus::SO3d SO3_q(q);              // 也可以通过四元数构造
   // 二者是等价的
@@ -31,7 +34,7 @@ int main(int argc, char **argv) {
   cout << "so3 hat vee= " << Sophus::SO3d::vee(Sophus::SO3d::hat(so3)).transpose() << endl;
 
   // 增量扰动模型的更新
-  Vector3d update_so3(1e-4, 0, 0); //假设更新量为这么多
+  Vector3d update_so3(1e-4, 0, 0); 
   Sophus::SO3d SO3_updated = Sophus::SO3d::exp(update_so3) * SO3_R;
   cout << "SO3 updated = \n" << SO3_updated.matrix() << endl;
 
@@ -44,6 +47,7 @@ int main(int argc, char **argv) {
   cout << "SE3 from q,t= \n" << SE3_qt.matrix() << endl;
   // 李代数se(3) 是一个六维向量，方便起见先typedef一下
   typedef Eigen::Matrix<double, 6, 1> Vector6d;
+  // 获得对应的李代数se(3)
   Vector6d se3 = SE3_Rt.log();
   cout << "se3 = " << se3.transpose() << endl;
   // 观察输出，会发现在Sophus中，se(3)的平移在前，旋转在后.
